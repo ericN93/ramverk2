729f402b67d572060e12faaa397116ff01485dc1
Eric Nilsson <eric.nilsson931211@gmail.com>
2017-10-31 15:13:25 +0100

    second commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+    res.render('index', { title: 'Me-sida' });
+});
+
+router.get('/about', function(req, res) {
+    res.render('about', {title: 'Om'});
+});
+
+router.get('/report', function(req, res) {
+    res.render('report', {title: 'Redovisning'});
 });
 
 module.exports = router;
